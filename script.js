877aba1e4b7d4c91dada259833b9e678306c5c76
Marlon Angulo <Marlonangulo001@gmail.com>
2022-11-17 19:56:40 +0000

    refactored one function into two

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function welcomePrompt(){
 
 alert("Welcome to Catz N Factz!");
@@ -5,14 +7,16 @@ let userName =  prompt("What is your name?");
 
     if (userName == ""){
         userName = prompt("You did not enter a name, what is your name?");
-        let proceed = confirm("Do you want to see some cats " + userName + "?");
-    } else{
-        let proceed = confirm("Do you want to see some cats " + userName + "?")
-    }
+    } 
+    return catsOrDogs(userName);
+}
+
+function catsOrDogs(userName){
+
+    let proceed = confirm("Do you want to see some cats " + userName + "?")
     if (proceed){
         alert( "Today's your lucky day " + userName + "! Enjoy the cute cat pictures!");
     }else{
-        window.location.href = ("https://www.dogtagart.com/blog/38-best-dog-instagram-accounts-follow-2021");
+        window.location.href = ("https://www.rd.com/list/dog-facts-you-didnt-know/");
     }
 }
-
